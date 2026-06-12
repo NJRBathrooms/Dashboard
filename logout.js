34bb9d866864bd48bf92f2edcc126bbebd9d4fef
NJRBathrooms <adminnjrbathrooms@gmail.com>
2026-06-12 11:54:19 -0300

    Delete logout.js

--- logout.js
+++
@@ -1,4 +0,0 @@
-module.exports = function handler(req, res) {
-  res.setHeader('Set-Cookie', 'njr_token=; HttpOnly; Path=/; SameSite=Strict; Max-Age=0');
-  return res.status(200).json({ ok: true });
-};
